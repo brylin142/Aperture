0749e71e6c74614f574953586fbce541b6819411
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-06-06 11:57:45 -0700

    Bootstrap current_user and add preloadedState

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
-  helper_method :logged_in?, :require_login
+  helper_method :logged_in?, :require_login, :current_user
 
   def login(user)
     @current_user = user
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,7 +6,21 @@ import Root from './components/root';
 import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser
+  } else {
+    store = configureStore();
+  }
+  
+  store = configureStore();
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
