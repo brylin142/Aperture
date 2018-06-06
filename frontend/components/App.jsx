bffcbbfac8ea68f08b340098eb69037c7de74740
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-06-05 21:48:07 -0700

    Add greeting containers

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Apertr</h1>
+  </div>
+);
+
+export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 import { signup, login, logout } from './util/session_api_util';
 
@@ -8,8 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
 
   window.getState = store.getState;
-window.dispatch = store.dispatch;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Flickr clone</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
