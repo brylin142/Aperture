b6755b1d71b56bb7d4122eb4936d0bbd6eb13c4a
brylin142 <33795878+brylin142@users.noreply.github.com>
2019-02-01 08:10:11 -0800

    Code cleanup and style changes

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-
-import { signup, login, logout } from './actions/session_actions';
-import { 
-  fetchPhotos, 
-  fetchPhoto, 
-  createPhoto, 
-  updatePhoto, 
-  deletePhoto, 
-  createComment,
-  deleteComment,
-  createTag,
-  deleteTag
-} from './actions/photo_actions';
   
 document.addEventListener("DOMContentLoaded", () => {
   let store;
@@ -30,18 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchPhotos = fetchPhotos;
-  window.fetchPhoto = fetchPhoto;
-  window.createPhoto = createPhoto;
-  window.updatePhoto = updatePhoto;
-  window.deletePhoto = deletePhoto;
-  window.createComment = createComment;
-  window.deleteComment = deleteComment;
-  window.createTag = createTag;
-  window.deleteTag = deleteTag;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
